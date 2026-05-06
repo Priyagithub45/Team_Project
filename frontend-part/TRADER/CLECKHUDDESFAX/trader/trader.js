c53905379c8c64511ac191bb0d726af2da5216ed
Binay Ghimire <binayire@gmail.com>
2026-05-06 21:13:18 +0545

    delete

--- frontend-part/TRADER/CLECKHUDDESFAX/trader/trader.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  console.log('Trader JS loaded');
-  
-  setTimeout(function() {
-    var el = document.querySelector('.success-message');
-    if(el) el.style.display = 'none';
-  }, 3000);
-});
